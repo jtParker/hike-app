991c546a57a644dbdfafe3ff781da416468549b0
JT Parker <jparkerdev@gmail.com>
2019-02-17 16:53:42 -0700

    map running

---
+++ src/MapView.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import ReactMapboxGl from 'react-mapbox-gl';
+import { Marker } from 'react-mapbox-gl';
+
+const Map = ReactMapboxGl({accessToken: "pk.eyJ1IjoianBhcmtlcmRldiIsImEiOiJjanF2dWo1Y28wejd5M3htaGg5aXdvbG45In0.67gA97wJ4xIWV9Mg5rYTxg"});
+
+class MapView extends Component {
+
+  render() {
+
+    return (
+      <div className="map-container">
+        <Map style="mapbox://styles/mapbox/streets-v8"
+          containerStyle={{
+            height: "600px",
+            width: "100%"
+            }}
+          center={[ -111.876183, 40.758701 ]}
+          zoom={[9]}
+          >
+        </Map>
+      </div>
+    )
+  }
+
+}
+
+export default MapView;
